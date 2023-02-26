9303396cb4955d212c59e1327cc4340b0df49a80
Patrick Schaak <patrick.kuehlen@yahoo.de>
2023-02-26 14:13:56 +0100

    Add project files.

---
+++ FirstTryCalculator/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FirstTryCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string content = (sender as Button).Content.ToString();
+
+            display.Text += content;
+        }       
+        
+        private int Equals(object sender, RoutedEventArgs e)
+        {
+            string writtenEquation = display.Text;
+
+            if (string.IsNullOrEmpty(writtenEquation))
+            {
+                return 0;
+            }
+
+
+
+            return 0;
+            
+        }
+    }
+}
