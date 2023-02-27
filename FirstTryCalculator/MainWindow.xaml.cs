c58ef9d029a3dfd9170327b704462a5973861737
Patrick Schaak <patrick.kuehlen@yahoo.de>
2023-02-27 19:47:39 +0100

    Trying to implement the Math Methods and the corresponding Events

--- FirstTryCalculator/MainWindow.xaml.cs
+++ FirstTryCalculator/MainWindow.xaml.cs
@@ -20,25 +20,128 @@ namespace FirstTryCalculator
     /// </summary>
     /// 
 
-
-
-    public delegate void MyDelegate(string msg);
-
     public partial class MainWindow : Window
     {
+        long digits;
+        long counter;
+        char operatingChar;
         public MainWindow()
         {
             InitializeComponent();
         }
 
+   
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             string content = (sender as Button).Content.ToString();
 
+
+            if (display.Text == "0")
+            {
+                display.Text = "";
+            }
+
             display.Text += content;
+
+        }        
+        
+        private void DeleteDigit(object sender, RoutedEventArgs e)
+        {
+            string content = (sender as Button).Content.ToString();
+
+            if (display.Text.Length == 1)
+            {
+                display.Text = "0";
+
+            }
+            else
+            {
+            
+                display.Text = display.Text.Remove(display.Text.Length -1);
+
+            }
+
+        }        
+        
+        private void Del_All(object sender, RoutedEventArgs e)
+        {
+
+
+            display.Text = "0";
+            counter = 0;
+            digits = 0;
 
         }
 
-       
+        private void Plus(object sender, RoutedEventArgs e)
+        {
+            long parsedNumber = Int32.Parse(display.Text);
+
+            operatingChar = '+';
+            display.Text = "0";
+
+            counter += parsedNumber;
+
+
+        }       
+        
+        private void Minus(object sender, RoutedEventArgs e)
+        {
+            long parsedNumber = Int32.Parse(display.Text);
+
+            operatingChar = '-';
+            display.Text = "0";
+
+            digits = parsedNumber;
+
+            counter -= (counter - digits);
+
+
+        }        
+        
+        private void Multiply(object sender, RoutedEventArgs e)
+        {
+            long parsedNumber = Int32.Parse(display.Text);
+
+            operatingChar = '*';
+            display.Text = "0";
+
+            digits = parsedNumber;
+            counter += (counter * digits);
+
+
+        }
+
+        private void Equation(object sender, RoutedEventArgs e)
+        {
+            long parsedNumber = Int32.Parse(display.Text);
+
+            if (operatingChar == '+')
+            {
+                long calculation = counter + parsedNumber;
+                display.Text = calculation.ToString();
+            
+            }
+            else if (operatingChar == '-')
+            {
+                long calculation = counter - parsedNumber;
+                display.Text = calculation.ToString();
+            }
+            else if (operatingChar == '/')
+            {
+                long calculation = counter / parsedNumber;
+                display.Text = calculation.ToString();
+            }
+            else if (operatingChar == '*')
+            {
+                long calculation = counter * parsedNumber;
+                display.Text = calculation.ToString();
+            }
+
+        }    
+        
+
     }
 }
+
+
