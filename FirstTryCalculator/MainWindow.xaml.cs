2cff4d17787db33e90c9496031d510b6a9ef94bf
Patrick Schaak <patrick.kuehlen@yahoo.de>
2023-02-28 18:45:18 +0100

    Implemented the mathemtical logic and improved the usability within the calculator by using booleans

--- FirstTryCalculator/MainWindow.xaml.cs
+++ FirstTryCalculator/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,9 +23,12 @@ namespace FirstTryCalculator
 
     public partial class MainWindow : Window
     {
-        long digits;
-        long counter;
+        long firstNumber;
+        long lastNumber;
+        long result;
         char operatingChar;
+
+        bool nextNumber = true;
         public MainWindow()
         {
             InitializeComponent();
@@ -35,19 +39,168 @@ namespace FirstTryCalculator
         {
             string content = (sender as Button).Content.ToString();
 
-
-            if (display.Text == "0")
+            if (nextNumber)
             {
                 display.Text = "";
+                nextNumber = false;
             }
 
             display.Text += content;
 
-        }        
-        
+        }
+        private void Plus(object sender, RoutedEventArgs e)
+        {
+
+            long parsedNumber = Int32.Parse(display.Text);
+            
+            operatingChar = '+';
+            
+            firstNumber = parsedNumber;
+            result = firstNumber + lastNumber;
+            lastNumber = result;
+
+            nextNumber = true;
+
+            display.Text = result.ToString();
+            
+        }
+
+        private void Minus(object sender, RoutedEventArgs e)
+        {
+            
+            long parsedNumber = Int32.Parse(display.Text);
+
+            if (operatingChar != '-')
+            {
+
+                lastNumber = parsedNumber;
+                operatingChar = '-';
+                nextNumber = true;
+
+            }
+            else 
+            { 
+            
+                firstNumber = parsedNumber;
+                result = lastNumber - firstNumber;
+                lastNumber = result;
+
+                nextNumber = true;
+
+                display.Text = result.ToString();
+            
+            }
+        }
+        private void Multiply(object sender, RoutedEventArgs e)
+        {
+            long parsedNumber = Int32.Parse(display.Text);
+
+            if (operatingChar != '*')
+            {
+
+                lastNumber = parsedNumber;
+                operatingChar = '*';
+                nextNumber = true;
+
+            }
+            else
+            {
+            
+                firstNumber = parsedNumber;
+                result = firstNumber * lastNumber;
+                lastNumber = result;
+            
+                nextNumber = true;
+            
+                display.Text = result.ToString();
+
+            }
+
+
+        }
+        private void Divide(object sender, RoutedEventArgs e)
+        {
+            
+            long parsedNumber = Int32.Parse(display.Text);
+
+            if (operatingChar != '/')
+            {
+
+                lastNumber = parsedNumber;
+                operatingChar = '/';
+                nextNumber = true;
+
+            }
+            else 
+            { 
+            
+                firstNumber = parsedNumber;
+                result = lastNumber / firstNumber;
+                lastNumber = result;
+
+                nextNumber = true;
+
+                display.Text = result.ToString();
+            
+            }
+        }
+        private void Equation(object sender, RoutedEventArgs e)
+        {
+            long parsedNumber = Int32.Parse(display.Text);
+
+            switch (operatingChar)
+            {
+                case '+':
+
+                    Plus(parsedNumber, e);
+                    break;
+                
+                case '-':
+                    
+                    Minus(parsedNumber, e);
+                    break; 
+                
+                case '*':
+                    
+                    Multiply(parsedNumber, e);
+                    break; 
+                
+                case '/':
+                    
+                    Divide(parsedNumber, e);
+                    break;
+            }
+
+
+            //if (operatingChar == '+')
+            //{
+            //    digits = parsedNumber;
+
+            //    counter += (counter + digits);
+            //    display.Text = counter.ToString();
+
+            //    digits = 0;
+
+            //}
+            //else if (operatingChar == '-')
+            //{
+            //    long calculation = counter - parsedNumber;
+            //    display.Text = calculation.ToString();
+            //}
+            //else if (operatingChar == '/')
+            //{
+            //    long calculation = counter / parsedNumber;
+            //    display.Text = calculation.ToString();
+            //}
+            //else if (operatingChar == '*')
+            //{
+            //    long calculation = counter * parsedNumber;
+            //    display.Text = calculation.ToString();
+            //}
+
+        }
         private void DeleteDigit(object sender, RoutedEventArgs e)
         {
-            string content = (sender as Button).Content.ToString();
 
             if (display.Text.Length == 1)
             {
@@ -56,90 +209,26 @@ namespace FirstTryCalculator
             }
             else
             {
-            
-                display.Text = display.Text.Remove(display.Text.Length -1);
+
+                display.Text = display.Text.Remove(display.Text.Length - 1);
 
             }
 
-        }        
-        
+        }
         private void Del_All(object sender, RoutedEventArgs e)
         {
 
-
             display.Text = "0";
-            counter = 0;
-            digits = 0;
+            firstNumber = 0;
+            lastNumber = 0;
+            result = 0;
 
         }
 
-        private void Plus(object sender, RoutedEventArgs e)
-        {
-            long parsedNumber = Int32.Parse(display.Text);
-
-            operatingChar = '+';
-            display.Text = "0";
-
-            counter += parsedNumber;
 
 
-        }       
-        
-        private void Minus(object sender, RoutedEventArgs e)
-        {
-            long parsedNumber = Int32.Parse(display.Text);
-
-            operatingChar = '-';
-            display.Text = "0";
-
-            digits = parsedNumber;
-
-            counter -= (counter - digits);
 
 
-        }        
-        
-        private void Multiply(object sender, RoutedEventArgs e)
-        {
-            long parsedNumber = Int32.Parse(display.Text);
-
-            operatingChar = '*';
-            display.Text = "0";
-
-            digits = parsedNumber;
-            counter += (counter * digits);
-
-
-        }
-
-        private void Equation(object sender, RoutedEventArgs e)
-        {
-            long parsedNumber = Int32.Parse(display.Text);
-
-            if (operatingChar == '+')
-            {
-                long calculation = counter + parsedNumber;
-                display.Text = calculation.ToString();
-            
-            }
-            else if (operatingChar == '-')
-            {
-                long calculation = counter - parsedNumber;
-                display.Text = calculation.ToString();
-            }
-            else if (operatingChar == '/')
-            {
-                long calculation = counter / parsedNumber;
-                display.Text = calculation.ToString();
-            }
-            else if (operatingChar == '*')
-            {
-                long calculation = counter * parsedNumber;
-                display.Text = calculation.ToString();
-            }
-
-        }    
-        
 
     }
 }
